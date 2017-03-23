1cfcb549fe48023af8f00f228c56ef09a1be6150
taihua <to760621@gmail.com>
2017-03-23 15:08:32 +0800

    安裝 https://github.com/webpack-contrib/extract-text-webpack-plugin 讓css產生為另外一個檔案 產生的檔案名稱為[name].css

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,27 @@
 var webpack = require('webpack');
 var path = require('path');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var inProduction = process.env.NODE_ENV === 'production'
 
 module.exports = {
-	entry: './src/main.js',
+	entry: {
+		app: './src/main.js'
+
+	},
 	output: {
 		path: path.resolve(__dirname,'./dist'),
-		filename: 'bundle.js'
+		filename: '[name].js'
 	},
 	module: {
 		rules : [
 			{
 				test: /\.s[ac]ss$/,
-				use: ['style-loader', 'css-loader', 'sass-loader']
+				use: ExtractTextPlugin.extract({
+					use: ['css-loader', 'sass-loader'],
+					fallback: 'style-loader'
+				})
 			},
-			{
-				test: /\.css$/,
-				use: ['style-loader', 'css-loader']
-			},
+
 			{
 				test: /\.js$/,
 				exclude: /node_modules/,
@@ -26,7 +30,9 @@ module.exports = {
 		]
 	},
 
-	plugins:[]
+	plugins:[
+		new ExtractTextPlugin('[name].css')
+	]
 };
 
 if (inProduction){
